ca6e76523a72e5a59e6c4e79921d45fd96dbae28
aridiahlestari <oiyayakkrid@gmail.com>
2023-05-30 14:51:26 +0800

    Menambahkan database dan menambahkan get users

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,24 +12,9 @@ public class Main {
     public static void main(String[] args) throws IOException {
         HttpServer httpServer = HttpServer.create(new InetSocketAddress("localhost", 5069), 0);
 
-        httpServer.createContext("/user", new UserHandler()); // Inisialisasi dengan UserHandler
+        httpServer.createContext("/", new Server()); // Inisialisasi dengan UserHandler
 
         httpServer.setExecutor(Executors.newSingleThreadExecutor());
         httpServer.start();
-    }
-
-    public static class UserHandler implements HttpHandler {
-
-        @Override
-        public void handle(HttpExchange exchange) throws IOException {
-            if ("GET".equals((exchange.getRequestMethod()))) {
-                String responseToBeSentBack = koneksiSQLite.Connection();
-                exchange.sendResponseHeaders(200, responseToBeSentBack.length());
-                OutputStream outputStream = exchange.getResponseBody();
-                outputStream.write(responseToBeSentBack.getBytes());
-                outputStream.flush();
-                outputStream.close();
-            }
-        }
     }
 }
